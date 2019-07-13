f840e82950f345a8d4558a7638aae43ebc02442b
Sysonn <sissonderek@icloud.com>
2019-07-13 18:23:01 -0400

    mobile alert pop-up

--- pages/amazon-redesign/scripts/load.js
+++ pages/amazon-redesign/scripts/load.js
@@ -2,12 +2,27 @@
 $(document).ready(function(){
    
     $('#content-block').load("home.html");
- 
+
+    
+   
+      setTimeout(function () {
+         var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+         if(width < 760){
+         $("#pop-overlay").velocity("transition.fadeIn", {duration: 500, delay: 0 });
+         }
+      }, 500);
+
  });
 
  function goNext(){
 
     $('#content-block').load("page2.html");
     window.history.pushState("Details", "Title", "/amazon-redesign/page2/");
+
+ }
+
+ function closepop(){
+   
+   $("#pop-overlay").velocity("transition.fadeOut", {duration: 500, delay: 0 });
 
  }
