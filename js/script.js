1c8dcb4fd692a6f286eab114fdfe3ae80a3d177b
Sysonn <sissonderek@icloud.com>
2019-07-13 22:36:15 -0400

    added contact page

--- js/script.js
+++ js/script.js
@@ -41,3 +41,14 @@ function goNext(){
     //window.history.pushState("Details", "Title", "/amazon-redesign/page2/");
 
  };
+
+ function loadContact(){
+
+    $("#root").velocity("transition.fadeOut", {duration: 500, delay: 0 });
+    setTimeout(function(){
+    $("#root").velocity("transition.fadeIn", {duration: 500, delay: 0 });
+    $('#root').load('./contact-me.html');
+    }, 500); 
+    //window.history.pushState("Details", "Title", "/amazon-redesign/page2/");
+
+ };
