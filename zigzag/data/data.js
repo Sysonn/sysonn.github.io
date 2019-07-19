3614ec4d26bb4bc51543dea98d434cc3fb89eafd
Sysonn <sissonderek@icloud.com>
2019-07-19 01:03:05 -0400

    zig zag responsive test

---
+++ zigzag/data/data.js
@@ -0,0 +1,89 @@
+
+const messagesData = [
+    {
+        id: 1,
+        text: "Yo.",
+        userbool: true,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 2,
+        text: "What's up.",
+        userbool: false,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 3,
+        text: "Nice.",
+        userbool: false,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 4,
+        text: "I'm enjoying this!",
+        userbool: true,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 5,
+        text: "Same.",
+        userbool: false,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 6,
+        text: "I love ZigZag.",
+        userbool: false,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 7,
+        text: "Dude, It's such a cool app.",
+        userbool: true,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 8,
+        text: "everyone should use it!",
+        userbool: false,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 9,
+        text: "Agreed.",
+        userbool: true,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 10,
+        text: "it's fun.",
+        userbool: true,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 11,
+        text: "it's cool ;)",
+        userbool: false,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 12,
+        text: "It's new!",
+        userbool: true,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 13,
+        text: "I love ZigZag!",
+        userbool: false,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    {
+        id: 14,
+        text: "No, I love ZigZag!",
+        userbool: true,
+        timestamp: "Jul 18 2019 11:34:43 PM",
+    },
+    
+    
+]
